df35bdd6246bbf262b942134a754d9fd0c42cbf1
liko <os-likui@jsfund.cn>
2019-06-13 17:37:53 +0800

    init project

---
+++ js/ad-killer.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    $("#asideFooter .aside-box")[0].remove();
+    $(".csdn-tracking-statistics.mb8.box-shadow").remove();
+    $("#kp_box_479")[0].remove();
+    $("#kp_box_480")[0].remove();
+    $("div[id^=_]").remove();
+    $(".recommend-item-box.recommend-ad-box").remove();
+    $("#dmp_ad_58")[0].remove();
+};
